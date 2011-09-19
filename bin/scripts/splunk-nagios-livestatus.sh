99f8ec58f4c5b559c00560c80b300f8b3b5c96f2
Luke Harris <skywalka@gmail.com>
2011-09-19 14:04:36 +0000

    Removed old livestatus config files and scripts - lukeh
    git-svn-id: https://splunk-for-nagios.googlecode.com/svn/branches/premier-1.2.0@115 625c67ee-5cd9-17ef-0f17-c7f45ccfe1e8

--- bin/scripts/splunk-nagios-livestatus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SPLUNK_HOME=/opt/splunk
-/usr/bin/nc 10.20.14.114 6557 < nagios-hosts > ../../lookups/nagios-hosts.csv
-/usr/bin/nc 10.20.14.114 6557 < nagios-servicegroups > ../../lookups/nagios-servicegroups.csv
-$SPLUNK_HOME/etc/apps/SplunkForNagios/bin/scripts/splunk-nagios-servicegroupmembers.sh > ../../lookups/nagios-servicegroupmembers.csv
-
--- bin/scripts/splunk-nagiosdev-livestatus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SPLUNK_HOME=/opt/splunk
-/usr/bin/nc 10.20.14.114 6558 < nagiosdev-hosts > ../../lookups/nagiosdev-hosts.csv
-/usr/bin/nc 10.20.14.114 6558 < nagiosdev-servicegroups > ../../lookups/nagiosdev-servicegroups.csv
-$SPLUNK_HOME/etc/apps/SplunkForNagios/bin/scripts/splunk-nagiosdev-servicegroupmembers.sh > ../../lookups/nagiosdev-servicegroupmembers.csv
-
