b2908569b8171c832166d801585024e8ead9a929
Luke Harris <skywalka@gmail.com>
2012-12-11 13:32:14 +1100

    Initial script to Schedule downtime for service problems Updated config files

---
+++ bin/liveservicedowntime.py
@@ -0,0 +1,50 @@
+# Script to set downtime for a service problem by accessing MK Livestatus
+# Required fields to be passed to this script from Splunk: src_host, name, starttime, endtime, comment
+import socket
+import sys,splunk.Intersplunk
+import string
+import datetime
+
+results = []
+
+try:
+
+    results,dummyresults,settings = splunk.Intersplunk.getOrganizedResults()
+
+    for r in results:
+        if "_raw" in r:
+            if "src_host" in r:
+                if "name" in r:
+                    try:
+		        HOST = 'nagios1'    # The remote nagios server
+		        PORT = 6557         # The remote port on the nagios server
+			nowepoch = datetime.datetime.now()
+			nowepoch2 = nowepoch.strftime("%s")
+			start = (r["starttime"])
+			end = (r["endtime"])
+			timeformat = "%Y-%m-%d %H:%M:%S"
+			st = datetime.datetime.strptime(start, timeformat)
+			st2 = int(st.strftime("%s"))
+			et = datetime.datetime.strptime(end, timeformat)
+			et2 = int(et.strftime("%s"))
+			duration = et2 - st2
+		        content = [ "COMMAND [", nowepoch2, "] SCHEDULE_SVC_DOWNTIME;", (r["src_host"]), ";", (r["name"]), ";", st2, ";", et2, ";1;0;", duration, ";nagiosadmin;", (r["comment"]), "\n" ]
+    		        query = "".join(map(str,content))
+		        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		        s.connect((HOST, PORT))
+		        s.send(query)
+		        s.shutdown(socket.SHUT_WR)
+		        data = s.recv(100000000)
+		        liveservicedowntime = string.split(data)
+		        s.close()
+                        r["liveservicedowntime"] = "Downtime Scheduled"
+                    except:
+                        r["liveservicedowntime"] = "Unknown"
+
+except:
+    import traceback
+    stack =  traceback.format_exc()
+    results = splunk.Intersplunk.generateErrorResults("Error : Traceback: " + str(stack))
+
+splunk.Intersplunk.outputResults( results )
+
