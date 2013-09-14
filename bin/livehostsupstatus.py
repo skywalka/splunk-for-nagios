99146979ca61c0c21f7bb713288dee2694c68877
Luke Harris <skywalka@gmail.com>
2013-09-14 14:53:49 +1000

    Added support for multiple MK Livestatus servers, included mklivestatus module, added field for total number of hosts

--- bin/livehostsupstatus.py
+++ bin/livehostsupstatus.py
@@ -1,8 +1,6 @@
-# Script to request all hosts with UP status by accessing MK Livestatus
-import socket
-import sys,splunk.Intersplunk
-import string
-import splunk4nagios
+# Script to request hosts with UP status and total hosts by accessing MK Livestatus
+# Required field to be passed to this script from Splunk: host (mk-livestatus/nagios server)
+import socket,string,sys,splunk.Intersplunk,mklivestatus
 
 results = []
 
@@ -12,22 +10,32 @@ try:
 
     for r in results:
         if "_raw" in r:
-            if "src_host" in r:
+            if "host" in r:
                     try:
-		        HOST = splunk4nagios.server    # The remote nagios server
-		        PORT = 6557              # The remote port on the nagios server
-		        content = [ "GET hosts\nStats: hard_state = 0\n" ]
-    		        query = "".join(content)
-		        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		        s.connect((HOST, PORT))
-		        s.send(query)
-		        s.shutdown(socket.SHUT_WR)
-		        data = s.recv(100000000)
-		        livehostsupstatus = string.split(data)
-		        s.close()
-                        r["livehostsupstatus"] = livehostsupstatus[0]
+			HOST = mklivestatus.HOST
+		        PORT = mklivestatus.PORT
+		        livehostsup = 0
+		        livehoststotal = 0
+    			for h in HOST:
+			    content = [ "GET hosts\nStats: state = 0\nStats: state != 9999\n" ]
+			    query = "".join(content)
+			    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			    s.connect((h, PORT))
+			    s.send(query)
+			    s.shutdown(socket.SHUT_WR)
+			    data = s.recv(100000000)
+			    livehosts2 = data.strip()
+			    livehosts = livehosts2.split(";")
+			    s.close()
+			    livehostsupind = int(livehosts[0])
+			    livehoststotalind = int(livehosts[1])
+			    livehostsup = livehostsup + livehostsupind
+			    livehoststotal = livehoststotal + livehoststotalind
+                        r["livehostsupstatus"] = livehostsup
+                        r["livehoststotalstatus"] = livehoststotal
                     except:
                         r["livehostsupstatus"] = "0"
+                        r["livehoststotalstatus"] = "0"
 
 except:
     import traceback
