cbf8e29022c80ad7c0388adabc8b47647d7972ff
Luke Harris <skywalka@gmail.com>
2013-09-14 14:55:23 +1000

    Added support for multiple MK Livestatus servers, included mklivestatus module, uses host field from search results to iterate through the MK Livestatus servers

--- bin/livehoststatus.py
+++ bin/livehoststatus.py
@@ -1,9 +1,6 @@
 # Script to set downtime for a host by accessing MK Livestatus
-# Required fields to be passed to this script from Splunk: src_host, starttime, endtime, comment
-import socket
-import sys,splunk.Intersplunk
-import string
-import splunk4nagios
+# Required field to be passed to this script from Splunk: host (mk-livestatus/nagios server)
+import socket,string,sys,splunk.Intersplunk,mklivestatus
 
 results = []
 
@@ -13,29 +10,22 @@ try:
 
     for r in results:
         if "_raw" in r:
-            if "src_host" in r:
-                    try:
-		        HOST = splunk4nagios.server    # The remote nagios server
-		        PORT = 6557         # The remote port on the nagios server
-		        content = [ "GET hosts\nFilter: host_name = ", (r["src_host"]), "\nColumns: name address alias hard_state\n" ]
-    		        query = "".join(map(str,content))
-		        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		        s.connect((HOST, PORT))
-		        s.send(query)
-		        s.shutdown(socket.SHUT_WR)
-		        data = s.recv(100000000)
-			livehoststatus2 = data.strip()
-			livehoststatus = livehoststatus2.split(";")
-		        s.close()
-                        r["src_host"] = livehoststatus[0]
-                        r["src_ip"] = livehoststatus[1]
-                        r["description"] = livehoststatus[2]
-                        r["livehoststatus"] = livehoststatus[3]
-                    except:
-                        r["src_host"] = "Unknown"
-                        r["src_ip"] = "Unknown"
-                        r["description"] = "Unknown"
-                        r["livehoststatus"] = "Unknown"
+            if "host" in r:
+                try:
+                    HOST = mklivestatus.HOST
+                    PORT = mklivestatus.PORT
+		    content = [ "GET hosts\nColumns: name address alias state\n" ]
+    		    query = "".join(map(str,content))
+		    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		    s.connect(((r["host"]), PORT))
+		    s.send(query)
+		    s.shutdown(socket.SHUT_WR)
+		    data = s.recv(100000000)
+		    table = data.split()
+		    s.close()
+    		    r["livehoststatus_results"] = table
+                except:
+                    r["livehoststatus_results"] = "Unknown"
 
 except:
     import traceback
