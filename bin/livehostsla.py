3527920e0d11755bcc1bfceaf5f2de4ab6e62f95
Luke Harris <skywalka@gmail.com>
2013-09-22 08:35:27 +1000

    Added support for multiple MK Livestatus servers, included mklivestatus module, uses host field from search results to iterate through the MK Livestatus servers

--- bin/livehostsla.py
+++ bin/livehostsla.py
@@ -1,8 +1,7 @@
-# Script to request a hosts' SLA by accessing MK Livestatus
-import socket
-import sys,splunk.Intersplunk
-import string
+# Script to request host SLA by accessing MK Livestatus
+import socket,string,mklivestatus
 from datetime import datetime, timedelta
+import sys,splunk.Intersplunk
 
 results = []
 
@@ -12,27 +11,27 @@ try:
 
     for r in results:
         if "_raw" in r:
-            if "src_host" in r:
+            if "host" in r:
                     try:
-		        HOST = 'nagios1'    # The remote nagios server
-		        PORT = 6557              # The remote port on the nagios server
+			PORT = mklivestatus.PORT
 			N = int(r["daysago"])
 			nowepoch = datetime.now()
 			nowepoch2 = nowepoch.strftime("%s")
 			date_N_days_ago = datetime.now() - timedelta(days=N)
 			date_N_days_ago2 = date_N_days_ago.strftime("%s")
-		        content = [ "GET statehist\nFilter: host_name = ", (r["src_host"]), "\nFilter: service_description = ", "\nFilter: time >= ", date_N_days_ago2, "\nFilter: time < ", nowepoch2, "\nStats: sum duration_part_ok", "\n" ]
-    		        query = "".join(content)
-		        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		        s.connect((HOST, PORT))
-		        s.send(query)
-		        s.shutdown(socket.SHUT_WR)
-		        data = s.recv(100000000)
-		        livehostsla = string.split(data)
-		        s.close()
-                        r["livehostsla"] = livehostsla[0]
+			content = [ "GET statehist\nColumns: host_name\nFilter: service_description =\nFilter: time >= ", date_N_days_ago2, "\nFilter: time < ", nowepoch2, "\nStats: sum duration_part_ok", "\n" ]
+			query = "".join(content)
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect(((r["host"]), PORT))
+			s.send(query)
+			s.shutdown(socket.SHUT_WR)
+			data = s.recv(100000000)
+			livehostsla2 = data.strip()
+			livehostsla = livehostsla2.split('\n')
+			s.close()
+                        r["livehostsla"] = livehostsla
                     except:
-                        r["livehostsla"] = "0"
+                        r["livehostsla"] = "UNKNOWN"
 
 except:
     import traceback
