7d6363c570cd4f06d034300426ac2760758f0917
Luke Harris <skywalka@gmail.com>
2013-09-22 15:43:14 +1000

    Added support for multiple MK Livestatus servers, included mklivestatus module, uses host field from search results to iterate through the MK Livestatus servers

--- bin/liveservicelistack.py
+++ bin/liveservicelistack.py
@@ -1,7 +1,6 @@
 # Script to list acknowledged service problems by accessing MK Livestatus
-import socket
+import socket,string,mklivestatus
 import sys,splunk.Intersplunk
-import string
 
 results = []
 
@@ -12,27 +11,21 @@ try:
     for r in results:
         if "_raw" in r:
             if "src_host" in r:
-                if "name" in r:
-                    try:
-		        HOST = 'nagios1'    # The remote nagios server
-		        PORT = 6557              # The remote port on the nagios server
-		        content = [ "GET services\nFilter: host_name = ", (r["src_host"]), "\nFilter: service_description = ", (r["name"]), "\nFilter: acknowledged = ", (r["acktype"]), "\nFilter: state != 0\nAnd: 2\nColumns: host_name service_description acknowledged\n" ]
-    		        query = "".join(content)
-		        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		        s.connect((HOST, PORT))
-		        s.send(query)
-		        s.shutdown(socket.SHUT_WR)
-		        data = s.recv(100000000)
-			liveservicelistack2 = data.strip()
-			liveservicelistack = liveservicelistack2.split(";")
-			s.close()
-                        r["src_host"] = liveservicelistack[0]
-                        r["name"] = liveservicelistack[1]
-                        r["liveservicelistack"] = liveservicelistack[2]
-                    except:
-                        r["src_host"] = "n/a"
-                        r["name"] = "n/a"
-                        r["liveservicelistack"] = "n/a"
+                try:
+		    PORT = mklivestatus.PORT
+		    content = [ "GET services\nFilter: acknowledged = ", (r["acktype"]), "\nFilter: state != 0\nAnd: 2\nColumns: host_name service_description acknowledged\n" ]
+    		    query = "".join(content)
+		    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		    s.connect(((r["host"]), PORT))
+		    s.send(query)
+		    s.shutdown(socket.SHUT_WR)
+		    data = s.recv(100000000)
+		    liveservicelistack2 = data.strip()
+		    liveservicelistack = liveservicelistack2.split("\n")
+		    s.close()
+                    r["liveservicelistack"] = liveservicelistack
+                except:
+                    r["liveservicelistack"] = "UNKNOWN"
 
 except:
     import traceback
