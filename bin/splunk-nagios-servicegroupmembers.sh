49cc18a27da660d42ad45086fdca6889e61854e2
Luke Harris <skywalka@gmail.com>
2013-09-24 23:47:31 +1000

    Updated example hostname

--- bin/splunk-nagios-servicegroupmembers.sh
+++ bin/splunk-nagios-servicegroupmembers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#### THIS FILE MANAGED BY PUPPET ####
 echo "host_name,name,state,num,servicegroup"
-/usr/bin/nc nagios-dev.noc.harvard.edu 6557 < nagios-servicegroupmembers |sed 's/,/\n/g'|sed 's/|/,/g'|while read line
+/usr/bin/nc nagios1 6557 < nagios-servicegroupmembers |sed 's/,/\n/g'|sed 's/|/,/g'|while read line
 do
 COUNT=`echo $line|grep -c ","`
 
@@ -14,4 +13,3 @@ fi
 
 done
 
-#### THIS FILE MANAGED BY PUPPET ####
