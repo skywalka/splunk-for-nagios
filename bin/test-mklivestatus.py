7a10a46558f49ac0e6ae26894af3c23e90d0d4e2
Luke Harris <skywalka@gmail.com>
2013-09-27 22:13:35 +1000

    Added exception catching

--- bin/test-mklivestatus.py
+++ bin/test-mklivestatus.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python
-import sys,string,socket,mklivestatus
+import sys,string,socket,re,mklivestatus
 HOST = mklivestatus.HOST
 PORT = mklivestatus.PORT
+s = None
 livehoststatus_results = []
 for h in HOST:
     content = [ "GET hosts\nColumns: name address alias state\n" ]
     query = "".join(map(str,content))
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((h, PORT))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((h, PORT))
+    except socket.error, (value,message):
+        if s: 
+	    s.close() 
+	    print "Error connecting to %s on port %d: " % (h, PORT) + message 
+	    break
     s.send(query)
     s.shutdown(socket.SHUT_WR)
     data = s.recv(100000000)
-    table = data.split()
-    s.close()
-    print table
+    data2 = (re.findall(r'(No UNIX socket)', data))
+    if data2:
+	print "Error connecting to %s on port %d: " % (h, PORT) + data
+	s.close()
+    else:
+    	table = data.split()
+    	s.close()
+	print table
