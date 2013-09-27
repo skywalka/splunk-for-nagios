f87accc97f8fbf4b1c3491c86ec9872149578681
Luke Harris <skywalka@gmail.com>
2013-09-27 21:15:00 +1000

    Added exception catching

--- bin/livehostsupstatus.py
+++ bin/livehostsupstatus.py
@@ -1,6 +1,6 @@
 # Script to request hosts with UP status and total hosts by accessing MK Livestatus
 # Required field to be passed to this script from Splunk: host (mk-livestatus/nagios server)
-import socket,string,sys,splunk.Intersplunk,mklivestatus
+import socket,string,sys,re,splunk.Intersplunk,mklivestatus
 
 results = []
 
@@ -14,23 +14,35 @@ try:
                     try:
 			HOST = mklivestatus.HOST
 		        PORT = mklivestatus.PORT
+			s = None 
 		        livehostsup = 0
 		        livehoststotal = 0
     			for h in HOST:
 			    content = [ "GET hosts\nStats: state = 0\nStats: state != 9999\n" ]
 			    query = "".join(content)
-			    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			    s.connect((h, PORT))
+    			    try:
+        			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        			s.connect((h, PORT))
+    			    except socket.error, (value,message): 
+        			if s: 
+			            s.close() 
+			            #Error: Could not open socket: connection refused (MK Livestatus not setup in xinetd?)
+			            break
 			    s.send(query)
 			    s.shutdown(socket.SHUT_WR)
 			    data = s.recv(100000000)
-			    livehosts2 = data.strip()
-			    livehosts = livehosts2.split(";")
-			    s.close()
-			    livehostsupind = int(livehosts[0])
-			    livehoststotalind = int(livehosts[1])
-			    livehostsup = livehostsup + livehostsupind
-			    livehoststotal = livehoststotal + livehoststotalind
+    			    data2 = (re.findall(r'(No UNIX socket)', data))
+			    if data2:
+			        #Error: MK Livestatus module not loaded?
+			        s.close()
+			    else:
+			        livehosts2 = data.strip()
+			        livehosts = livehosts2.split(";")
+			        s.close()
+			        livehostsupind = int(livehosts[0])
+			        livehoststotalind = int(livehosts[1])
+			        livehostsup = livehostsup + livehostsupind
+			        livehoststotal = livehoststotal + livehoststotalind
                         r["livehostsupstatus"] = livehostsup
                         r["livehoststotalstatus"] = livehoststotal
                     except:
